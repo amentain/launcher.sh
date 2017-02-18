25ab6fd0b4ae60c84db166c7b55b32760cd28dfe
Pavel N. Savenkov <xeenon@me.com>
2017-02-18 16:01:19 +0300

    update sample

--- launcher.sh
+++ launcher.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-xdl_conf="some.daemon.ini"
+workingdir=$(cd "$(cd "$(dirname "${BASH_SOURCE}")"; pwd -P)"; pwd)
+xdl_conf="${workingdir}/launcher.sample.ini"
 
 #if [ $# -gt 0 -a $(id -un) != "www" ]; then
 #    printf "Using sudo -u www...\n\n\n"
@@ -8,4 +9,4 @@ xdl_conf="some.daemon.ini"
 #    exit $?
 #fi
 
-. ./lib.launcher.sh
+. ${workingdir}/lib.launcher.sh
